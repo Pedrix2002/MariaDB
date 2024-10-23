d674da5328477e36993b0459a4b9117e17fce0d4
Pedrix2002 <pdeleonc5@miumg.edu.gt>
2024-10-22 21:35:47 -0600

    Create Producto.sql

---
+++ Producto.sql
@@ -0,0 +1,3 @@
+INSERT INTO productos (nombre_producto, precio) VALUES ('Laptop', 800.50);
+INSERT INTO productos (nombre_producto, precio) VALUES ('Smartphone', 600.00);
+INSERT INTO productos (nombre_producto, precio) VALUES ('Teclado', 25.75);
